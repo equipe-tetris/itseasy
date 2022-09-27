d3a9a44e0ef94d182bb33184841c3f1b3ea2a890
gabrielsantos741 <62222361+gabrielsantos741@users.noreply.github.com>
2022-09-27 13:10:49 -0300

    Subindo arquivos do back-end

---
+++ back-end/ITSeasy-spring-boot-app/src/main/java/br/gov/sp/fatec/ITSeasyspringbootapp/ItSeasySpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.ITSeasyspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItSeasySpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItSeasySpringBootAppApplication.class, args);
+	}
+
+}
---
+++ back-end/ITSeasy-spring-boot-app/src/test/java/br/gov/sp/fatec/ITSeasyspringbootapp/ItSeasySpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.ITSeasyspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItSeasySpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
